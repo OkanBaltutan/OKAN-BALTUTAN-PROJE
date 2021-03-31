6bc74c825efd31b4590d0d60a546a9aa30fdb54c
OkanBaltutan <okan.baltutan@yahoo.com>
2021-03-31 23:16:52 +0300

    Initial commit

---
+++ LibraryOB/LibraryOB/Model/Books.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryOB.Core.Model
+{
+    public class Books
+    {
+        public int BookID { get; set; }
+        public string BookName { get; set; }
+        public string BookWriter { get; set; }
+        public string BookPublished { get; set; }
+        public int BookPages { get; set; }
+        public string BookLanguage { get; set; }
+        public string BookGenre { get; set; }
+    }
+}
---
+++ LibraryOB/LibraryOB/Model/Member.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryOB.Core.Model
+{
+    public class Member
+    {
+        public int MemberID { get; set; }
+        public string MemberName { get; set; }
+        public string MemberSurname { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string HomeAddress { get; set; }
+    }
+}
